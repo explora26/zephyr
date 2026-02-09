f706c217b52fb157e3e8b2ac68fd209412a9f195
Harry Jiang <explora26@gmail.com>
2026-02-09 22:56:36 +0800

    h750 wip, not work

---
+++ boards/weact/mini_stm32h750/board.cmake
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+if(CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT)
+  board_runner_args(stm32cubeprogrammer "--extload=mini_stm32h750_ext_loader.stldr")
+endif()
+
+board_runner_args(stlink_gdbserver "--extload=mini_stm32h750_ext_loader.stldr")
+board_runner_args(jlink "--device=STM32H750VB" "--speed=4000")
+board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
