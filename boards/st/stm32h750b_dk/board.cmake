3f4a9e514252fe30a5f4b2c976bcf009c36ca644
Harry Jiang <explora26@gmail.com>
2026-02-09 22:54:11 +0800

    external loader work

--- boards/st/stm32h750b_dk/board.cmake
+++ boards/st/stm32h750b_dk/board.cmake
@@ -4,10 +4,10 @@
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 if(CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT)
-  board_runner_args(stm32cubeprogrammer "--extload=MT25TL01G_STM32H750B-DISCO.stldr")
+  board_runner_args(stm32cubeprogrammer "--extload=new_stm32h750_extloader.stldr")
 endif()
 
-board_runner_args(stlink_gdbserver "--extload=MT25TL01G_STM32H750B-DISCO.stldr")
+board_runner_args(stlink_gdbserver "--extload=new_stm32h750_extloader.stldr")
 
 board_runner_args(jlink "--device=STM32H735IG" "--speed=4000")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
--- boards/weact/mini_stm32h743/board.cmake
+++ boards/weact/mini_stm32h743/board.cmake
@@ -1,6 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+if(CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT)
+  board_runner_args(stm32cubeprogrammer "--extload=mini_stm32h750_ext_loader.stldr")
+endif()
+
+board_runner_args(stlink_gdbserver "--extload=mini_stm32h750_ext_loader.stldr")
 board_runner_args(jlink "--device=STM32H735IG" "--speed=4000")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
